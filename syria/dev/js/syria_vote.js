83204c62397e28f3a8e541bce8c5a452a10b623e
Ben Breedlove <benbreedlove@gmail.com>
2013-09-05 17:01:11 -0700

    fuck that vis. use this one instead

--- syria/dev/js/syria_vote.js
+++ syria/dev/js/syria_vote.js
@@ -7,171 +7,232 @@
  */
 "use strict";
 
+var current_d = 199;
+var current_r = 234;
+var current_empty = 2;
+var total_seats = 435;
+
+
+var tooltip_template = ' \
+    <h3>{member}</h3> \
+    <p>Party Affiliation: {party}</p> \
+    <p>{strength} {lean}</p> \
+';
+var compiled_tooltip = dust.compile(tooltip_template, 'tooltip');
+dust.loadSource(compiled_tooltip);
+
+
+
 window.onload = function() {
 
         Tabletop.init( { 
             key: public_spreadsheet_url,
             callback: function(dataset) {
-                var root = transform_csv_data(dataset);
-                count_whip(root);
+                var leaning = cleanup_dataset(dataset)
+                count_whip(leaning);
             },
             simpleSheet: true 
         } )
 };
 
 
-var transform_csv_data = function(dataset) {
-    var placement = {
-        R : {
-            yes:{
+
+var cleanup_dataset = function( dataset ) {
+    console.log('dataset is this long: ' + dataset.length)
+    var leaning = {
+        D: {
+            yes : {
                 strong: [],
-                weak: []
+                weak: [],
             },
-            no:{
+            no : {
                 strong: [],
-                weak: []
+                weak: [],
             },
-            undecided: [],
-            unknown: []
+            unknown : [],
+            undecided : [],
         },
-        D : {
-            yes:{
+        R: {
+            yes : {
                 strong: [],
-                weak: []
+                weak: [],
             },
-            no:{
+            no : {
                 strong: [],
-                weak: []
+                weak: [],
             },
-            undecided: [],
-            unknown: []
-        }
-    }
+            unknown : [],
+            undecided : [],
+        },
+        I: {
+            yes : {
+                strong: [],
+                weak: [],
+            },
+            no : {
+                strong: [],
+                weak: [],
+            },
+            unknown : [],
+            undecided : [],
+        },
+    };
+
+    var num_d = 0;
+    var num_r = 0;
+    var num_i = 0;
+
     for (var i = 0; i < dataset.length; i++) {
-        var leg = dataset[i];
-        var yn = (leg.undecided)
+        var lean = dataset[i].lean = (dataset[i].undecided)
             ? 'undecided'
-            : (leg.unknown)
+            : (dataset[i].unknown)
                 ? 'unknown'
-                : (leg.no || leg.leanno)
+                : (dataset[i].no || dataset[i].leanno)
                     ? 'no'
                     : 'yes'
-        var strength = (leg.no || leg.yes) ? 'strong' : 'weak';
-        var leg_obj = {
-            "name" : leg.member,
-            "size" : 400,
-            "party" : leg.party,
-            "leaning" : strength + yn
-        };
-        if (yn === 'undecided' || yn === 'unknown') {
-            //placement[leg.party][yn].push(leg_obj)
+        var strength = dataset[i].strength = (dataset[i].undecided || dataset[i].unknown)
+            ? ''
+            : (dataset[i].no || dataset[i].yes) 
+                ? 'strong' 
+                : 'weak';
+        if (dataset[i].party === 'D') {
+            num_d++;
+        } else if (dataset[i].party === 'R') {
+            num_r++;
+        } else if (dataset[i].party === 'I') {
+            console.log('no party');
+            console.log(dataset[i]);
+            num_i++;
+        }
+
+        dust.render('tooltip', dataset[i], function(err, out) {
+            dataset[i].tooltip = out;
+        })
+
+        var member = { 
+            name: dataset[i].member,
+            party: dataset[i].party,
+            lean: lean + strength, 
+            tooltip: dataset[i].tooltip
+        }
+        if (lean === 'undecided' || lean === 'unknown') {
+            leaning[dataset[i].party][lean].push(member)
         } else {
-            placement[leg.party][yn][strength].push(leg_obj)
+            leaning[dataset[i].party][lean][strength].push(member);
         }
 
     }
-    var root = {
-        "name": "legislators",
-        "children": [
-            {
-                "name": "democratic",
-                "children": [
-                    {
-                        "name" : "unknown",
-                        "children" : placement.D.unknown
-                    },
-                    {
-                        "name" : "undecided",
-                        "children" : placement.D.undecided
-                    },
-                    {
-                        "name": "yes",
-                        "children": [
-                            {
-                                "name": "strong",
-                                "children" : placement.D.yes.strong
-                            },
-                            {
-                                "name": "weak",
-                                "children" : placement.D.yes.weak
-                            }
-                        ],
-                    },
-                    {
-                        "name": "no",
-                        "children": [
-                            {
-                                "name": "strong",
-                                "children" : placement.D.no.strong
-                            },
-                            {
-                                "name": "weak",
-                                "children" : placement.D.no.weak
-                            }
-                        ],
-                    }
-                ],
-            },
-            {
-                "name": "republican",
-                "children": [
-                    {
-                        "name" : "unknown",
-                        "children" : placement.R.unknown
-                    },
-                    {
-                        "name" : "undecided",
-                        "children" : placement.R.undecided
-                    },
-                    {
-                        "name": "yes",
-                        "children": [
-                            {
-                                "name": "strong",
-                                "children" : placement.R.yes.strong
-                            },
-                            {
-                                "name": "weak",
-                                "children" : placement.R.yes.weak
-                            }
-                        ],
-                    },
-                    {
-                        "name": "no",
-                        "children": [
-                            {
-                                "name": "strong",
-                                "children" : placement.R.no.strong
-                            },
-                            {
-                                "name": "weak",
-                                "children" : placement.R.no.weak
-                            }
-                        ],
-                    }
-                ],
-            },
+    console.log(num_d);
+    console.log(num_r);
+    return leaning;
+};
 
-        ],
-    };
-    return root;
-}
+var count_whip = function(leaning) {
+    var strong_d_no = leaning.D.no.strong;
+    for (var i = 0; i < strong_d_no.length; i++) {
+        var circle = document.getElementById('seat' + i);
+        circle.className.baseVal = 'strongDno';
+        circle.setAttribute('data-tooltip', strong_d_no[i].tooltip);
+    }
+    console.log('stops at ' + i)
 
-var count_whip = function( root ) {
-  var node = svg.datum(root).selectAll(".node")
-      .data(pack.nodes)
-    .enter().append("g")
-      .attr("class", function(d) { console.log(d); return d.children ? d.name : d.leaning + d.party; })
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+    var weak_d_no = leaning.D.no.weak;
+    for (var i = 0; i < weak_d_no.length; i++) {
+        var circle = document.getElementById('seat' + ((i + 0) + strong_d_no.length));
+        circle.className.baseVal = 'weakDno';
+        circle.setAttribute('data-tooltip', weak_d_no[i].tooltip);
+    }
 
-  node.append("title")
-      .text(function(d) { return d.name + (d.children ? "" : ": " + format(d.size)); });
+    var strong_r_no = leaning.R.no.strong;
+    for (var i = 0; i < strong_r_no.length; i++) {
+        var circle = document.getElementById('seat' + (total_seats - i - 1));
+        circle.className.baseVal = 'strongRno';
+        circle.setAttribute('data-tooltip', strong_r_no[i].tooltip );
+    }
+    var weak_r_no = leaning.R.no.weak;
+    for (var i = 0; i < weak_r_no.length; i++) {
+        var circle = document.getElementById('seat' + (total_seats - i - strong_r_no.length - 1));
+        console.log(total_seats - i - strong_r_no.length - 1);
+        circle.className.baseVal = 'weakRno';
+        circle.setAttribute('data-tooltip', weak_r_no[i].tooltip);
+    }
 
-  node.append("circle")
-      .attr("r", function(d) { return d.r; });
+    var d_yes_start_point = current_d - 1;
+    var strong_d_yes = leaning.D.yes.strong;
+    for (var i = 0; i < strong_d_yes.length; i++) {
+        var circle = document.getElementById('seat' + (d_yes_start_point - i));
+        circle.className.baseVal = 'strongDyes';
+        circle.setAttribute('data-tooltip', strong_d_yes[i].tooltip);
+    }
 
-  node.filter(function(d) { return !d.children; }).append("text")
-      .attr("dy", ".3em")
-      .style("text-anchor", "middle")
-      .text(function(d) { return d.name.substring(0, d.r / 3); });
+    var weak_d_yes = leaning.D.yes.weak;
+    for (var i = 0; i < weak_d_yes.length; i++) {
+        var circle = document.getElementById('seat' + (d_yes_start_point - i - strong_d_yes.length));
+        circle.className.baseVal = 'weakDyes';
+        circle.setAttribute('data-tooltip', weak_d_yes[i].tooltip);
+    }
+
+    var r_yes_start_point = total_seats - current_r;
+    var strong_r_yes = leaning.R.yes.strong;
+    for (var i = 0; i < strong_r_yes.length; i++) {
+        var circle = document.getElementById('seat' + (r_yes_start_point + i));
+        circle.className.baseVal = 'strongRyes';
+        circle.setAttribute('data-tooltip', strong_r_yes[i].tooltip );
+    }
+
+    var weak_r_yes = leaning.R.yes.weak;
+    for (var i = 0; i < weak_r_yes.length; i++) {
+        var circle = document.getElementById('seat' + (r_yes_start_point + i + strong_r_yes.length));
+        circle.className.baseVal = 'weakRyes';
+        circle.setAttribute('data-tooltip', weak_r_yes[i].tooltip);
+    }
+
+    var d_un_start_point = weak_d_no.length + weak_d_yes.length + 2;
+    console.log('starts at ' + d_un_start_point + 2)
+    var d_undecided = leaning.D.undecided;
+    for (var i = 0; i < d_undecided.length; i++) {
+        var circle = document.getElementById('seat' + (d_un_start_point + i + 1));
+        circle.className.baseVal = 'd_undecided';
+        circle.setAttribute('data-tooltip', d_undecided[i].tooltip);
+    }
+    var d_unknown = leaning.D.unknown;
+    for (var i = 0; i < d_unknown.length; i++) {
+        var circle = document.getElementById('seat' + (d_un_start_point + i + d_undecided.length + 1));
+        circle.className.baseVal = 'd_unknown';
+        circle.setAttribute('data-tooltip', d_unknown[i].tooltip);
+    }
+
+    var r_un_start_point = total_seats - 1 - (weak_r_no.length + strong_r_no.length);
+    var r_undecided = leaning.R.undecided;
+    for (var i = 0; i < r_undecided.length; i++) {
+        var circle = document.getElementById('seat' + (r_un_start_point - i));
+        circle.className.baseVal = 'r_undecided';
+        circle.setAttribute('data-tooltip', r_undecided[i].tooltip);
+    }
+    var r_unknown = leaning.R.unknown;
+    for (var i = 0; i < r_unknown.length; i++) {
+        var circle = document.getElementById('seat' + (r_un_start_point - i - r_undecided.length));
+        circle.className.baseVal = 'r_unknown';
+        circle.setAttribute('data-tooltip', r_unknown[i].tooltip + r_unknown[i].lean);
+    }
+
+    document.getElementById('seat199').className.baseVal = 'empty_seat';
+    document.getElementById('seat200').className.baseVal = 'empty_seat';
+    document.getElementById('seat199').setAttribute('data-tooltip', 'Empty Seat');
+    document.getElementById('seat200').setAttribute('data-tooltip', 'Empty Seat');
+
+      var tooltip = jQuery('<div id="tooltip" style="display: none; position: absolute;"><p>text</p></div>');
+      jQuery('svg').before(tooltip);
+
+      jQuery('svg circle').bind('mouseout', function(){
+          tooltip.css('display', 'none');
+      });
+      jQuery('svg circle').bind('mousemove', function(e){
+          tooltip.css('left', e.pageX + 10)
+            .css('top', e.pageY + 10)
+            .css('display', 'block')
+            .html(jQuery(this).attr('data-tooltip'));
+      });
+
+
 };
