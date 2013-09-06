fb1579f340639b9d0ae66e1fc7566e6218afb0d7
Ben Breedlove <benbreedlove@gmail.com>
2013-09-06 11:10:58 -0700

    make seat finding easier, add buttons to see different sets

--- dev/js/syria_vote.js
+++ dev/js/syria_vote.js
@@ -21,18 +21,46 @@ var tooltip_template = ' \
 var compiled_tooltip = dust.compile(tooltip_template, 'tooltip');
 dust.loadSource(compiled_tooltip);
 
-
+var tooltip_element = jQuery('<div id="tooltip" style="display: none; position: absolute;"><p>text</p></div>');
 
 window.onload = function() {
+    jQuery('svg').before(tooltip_element);
 
+    var present_dataset;
         Tabletop.init( { 
             key: public_spreadsheet_url,
             callback: function(dataset) {
-                var leaning = cleanup_dataset(dataset)
+                var leaning = present_dataset = cleanup_dataset(dataset)
+                console.log('current dataset has ' + dataset.length); 
                 count_whip(leaning);
             },
             simpleSheet: true 
         } )
+
+    var sep_5_6pm_title = [];
+    for(var k in sep_5_6pm) sep_5_6pm_title.push(k);
+    sep_5_6pm_title = sep_5_6pm_title[0]
+    console.log('sep 5 dataset has ' + sep_5_6pm[sep_5_6pm_title].length); 
+
+    var sep_6_12am_title = [];
+    for(var k in sep_6_12am) sep_6_12am_title.push(k);
+    sep_6_12am_title = sep_6_12am_title[0]
+    console.log('sep 6 dataset has ' + sep_6_12am[sep_6_12am_title].length); 
+
+
+    var sep_5_6pm_cleaned = cleanup_dataset(sep_5_6pm[sep_5_6pm_title])
+    var sep_6_12am_cleaned = cleanup_dataset(sep_6_12am[sep_6_12am_title])
+
+    jQuery('#sep5_6pm').click(function() {
+        count_whip(sep_5_6pm_cleaned);
+    });
+    jQuery('#sep6_12am').click(function() {
+        count_whip(sep_6_12am_cleaned);
+    });
+    jQuery('#current').click(function() {
+        count_whip(present_dataset);
+    });
+
 };
 
 
@@ -123,104 +151,123 @@ var cleanup_dataset = function( dataset ) {
 };
 
 var count_whip = function(leaning) {
+    var seat_count = 0;
+
     var strong_d_no = leaning.D.no.strong;
     for (var i = 0; i < strong_d_no.length; i++) {
-        var circle = document.getElementById('seat' + i);
-        circle.className.baseVal = 'strongDno';
+        var circle = document.getElementById('seat' + seat_count);
+        seat_count++;
+        circle.setAttribute('class', 'strongDno ' + strong_d_no[i].name.replace(/\s/, ''));
         circle.setAttribute('data-tooltip', strong_d_no[i].tooltip);
     }
 
     var weak_d_no = leaning.D.no.weak;
     for (var i = 0; i < weak_d_no.length; i++) {
-        var circle = document.getElementById('seat' + ((i + 0) + strong_d_no.length));
-        circle.className.baseVal = 'weakDno';
+        var circle = document.getElementById('seat' + seat_count);
+        seat_count++;
+        circle.setAttribute('class', 'weakDno ' + weak_d_no[i].name.replace(/\s/, ''));
         circle.setAttribute('data-tooltip', weak_d_no[i].tooltip);
     }
 
-    var strong_r_no = leaning.R.no.strong;
-    for (var i = 0; i < strong_r_no.length; i++) {
-        var circle = document.getElementById('seat' + (total_seats - i - 1));
-        circle.className.baseVal = 'strongRno';
-        circle.setAttribute('data-tooltip', strong_r_no[i].tooltip );
-    }
-    var weak_r_no = leaning.R.no.weak;
-    for (var i = 0; i < weak_r_no.length; i++) {
-        var circle = document.getElementById('seat' + (total_seats - i - strong_r_no.length - 1));
-        circle.className.baseVal = 'weakRno';
-        circle.setAttribute('data-tooltip', weak_r_no[i].tooltip);
-    }
-
-    var d_yes_start_point = current_d - 1;
-    var strong_d_yes = leaning.D.yes.strong;
-    for (var i = 0; i < strong_d_yes.length; i++) {
-        var circle = document.getElementById('seat' + (d_yes_start_point - i));
-        circle.className.baseVal = 'strongDyes';
-        circle.setAttribute('data-tooltip', strong_d_yes[i].tooltip);
-    }
-
-    var weak_d_yes = leaning.D.yes.weak;
-    for (var i = 0; i < weak_d_yes.length; i++) {
-        var circle = document.getElementById('seat' + (d_yes_start_point - i - strong_d_yes.length));
-        circle.className.baseVal = 'weakDyes';
-        circle.setAttribute('data-tooltip', weak_d_yes[i].tooltip);
-    }
-
-    var r_yes_start_point = total_seats - current_r;
-    var strong_r_yes = leaning.R.yes.strong;
-    for (var i = 0; i < strong_r_yes.length; i++) {
-        var circle = document.getElementById('seat' + (r_yes_start_point + i));
-        circle.className.baseVal = 'strongRyes';
-        circle.setAttribute('data-tooltip', strong_r_yes[i].tooltip );
-    }
-
-    var weak_r_yes = leaning.R.yes.weak;
-    for (var i = 0; i < weak_r_yes.length; i++) {
-        var circle = document.getElementById('seat' + (r_yes_start_point + i + strong_r_yes.length));
-        circle.className.baseVal = 'weakRyes';
-        circle.setAttribute('data-tooltip', weak_r_yes[i].tooltip);
-    }
-
-    var d_un_start_point = weak_d_no.length + weak_d_yes.length + 2;
     var d_undecided = leaning.D.undecided;
     for (var i = 0; i < d_undecided.length; i++) {
-        var circle = document.getElementById('seat' + (d_un_start_point + i + 1));
-        circle.className.baseVal = 'd_undecided';
+        var circle = document.getElementById('seat' + seat_count);
+        seat_count++;
+        circle.setAttribute('class', 'd_undecided ' + d_undecided[i].name.replace(/\s/, ''));
         circle.setAttribute('data-tooltip', d_undecided[i].tooltip);
     }
     var d_unknown = leaning.D.unknown;
     for (var i = 0; i < d_unknown.length; i++) {
-        var circle = document.getElementById('seat' + (d_un_start_point + i + d_undecided.length + 1));
-        circle.className.baseVal = 'd_unknown';
+        var circle = document.getElementById('seat' + seat_count);
+        seat_count++;
+        circle.setAttribute('class', 'd_unknown ' + d_unknown[i].name.replace(/\s/, ''));
         circle.setAttribute('data-tooltip', d_unknown[i].tooltip);
     }
 
-    var r_un_start_point = total_seats - 1 - (weak_r_no.length + strong_r_no.length);
-    var r_undecided = leaning.R.undecided;
-    for (var i = 0; i < r_undecided.length; i++) {
-        var circle = document.getElementById('seat' + (r_un_start_point - i));
-        circle.className.baseVal = 'r_undecided';
-        circle.setAttribute('data-tooltip', r_undecided[i].tooltip);
+    var weak_d_yes = leaning.D.yes.weak;
+    for (var i = 0; i < weak_d_yes.length; i++) {
+        var circle = document.getElementById('seat' + seat_count);
+        seat_count++;
+        circle.setAttribute('class', 'weakDyes ' + weak_d_yes[i].name.replace(/\s/, ''));
+        circle.setAttribute('data-tooltip', weak_d_yes[i].tooltip);
     }
+
+    var strong_d_yes = leaning.D.yes.strong;
+    for (var i = 0; i < strong_d_yes.length; i++) {
+        var circle = document.getElementById('seat' + seat_count);
+        circle.setAttribute('class', 'strongDyes ' + seat_count);
+        seat_count++;
+        circle.setAttribute('data-tooltip', strong_d_yes[i].tooltip);
+    }
+
+    document.getElementById('seat199').setAttribute('class', 'empty_seat');
+    document.getElementById('seat199').setAttribute('data-tooltip', 'Empty Seat seat 199');
+    seat_count++;
+    document.getElementById('seat200').setAttribute('class', 'empty_seat');
+    document.getElementById('seat200').setAttribute('data-tooltip', 'Empty Seat seat 200');
+    seat_count++;
+
+    var strong_r_yes = leaning.R.yes.strong;
+    for (var i = 0; i < strong_r_yes.length; i++) {
+        var circle = document.getElementById('seat' + seat_count);
+        seat_count++;
+        circle.setAttribute('class', 'strongRyes ' + strong_r_yes[i].name.replace(/\s/, ''));
+        circle.setAttribute('data-tooltip', strong_r_yes[i].tooltip );
+    }
+
+    var weak_r_yes = leaning.R.yes.weak;
+    console.log('weak r ' + weak_r_yes.length);
+    for (var i = 0; i < weak_r_yes.length; i++) {
+        var circle = document.getElementById('seat' + seat_count);
+        seat_count++;
+        circle.setAttribute('class', 'weakRyes ' + weak_r_yes[i].name.replace(/\s/, ''));
+        circle.setAttribute('data-tooltip', weak_r_yes[i].tooltip);
+    }
+
+    var r_un_start_point = r_yes_start_point + strong_r_yes.length + weak_r_yes.length;
     var r_unknown = leaning.R.unknown;
+    console.log('r unkown ' + r_unknown.length);
     for (var i = 0; i < r_unknown.length; i++) {
-        var circle = document.getElementById('seat' + (r_un_start_point - i - r_undecided.length));
-        circle.className.baseVal = 'r_unknown';
+        var circle = document.getElementById('seat' + seat_count);
+        seat_count++;
+        circle.setAttribute('class', 'r_unknown ' + r_unknown[i].name.replace(/\s/, ''));
         circle.setAttribute('data-tooltip', r_unknown[i].tooltip + r_unknown[i].lean);
     }
 
-    document.getElementById('seat199').className.baseVal = 'empty_seat';
-    document.getElementById('seat200').className.baseVal = 'empty_seat';
-    document.getElementById('seat199').setAttribute('data-tooltip', 'Empty Seat');
-    document.getElementById('seat200').setAttribute('data-tooltip', 'Empty Seat');
+    //var r_un_start_point = total_seats - 1 - (weak_r_no.length + strong_r_no.length);
+    var r_undecided = leaning.R.undecided;
+    for (var i = 0; i < r_undecided.length; i++) {
+        var circle = document.getElementById('seat' + seat_count);
+        seat_count++;
+        circle.setAttribute('class', 'r_undecided ' + r_undecided[i].name.replace(/\s/, ''));
+        circle.setAttribute('data-tooltip', r_undecided[i].tooltip);
+    }
+    
 
-      var tooltip = jQuery('<div id="tooltip" style="display: none; position: absolute;"><p>text</p></div>');
-      jQuery('svg').before(tooltip);
+    var r_yes_start_point = r_un_start_point + r_unknown.length + r_undecided.length;
+    var weak_r_no = leaning.R.no.weak;
+    for (var i = 0; i < weak_r_no.length; i++) {
+        var circle = document.getElementById('seat' + seat_count);
+        seat_count++;
+        circle.setAttribute('class', 'weakRno ' + weak_r_no[i].name.replace(/\s/, ''));
+        circle.setAttribute('data-tooltip', weak_r_no[i].tooltip);
+    }
+
+    var strong_r_no = leaning.R.no.strong;
+    for (var i = 0; i < strong_r_no.length; i++) {
+        var circle = document.getElementById('seat' + seat_count);
+        seat_count++;
+        circle.setAttribute('class', 'strongRno ' + strong_r_no[i].name.replace(/\s/, ''));
+        circle.setAttribute('data-tooltip', strong_r_no[i].tooltip );
+    }
+
+
 
       jQuery('svg circle').bind('mouseout', function(){
-          tooltip.css('display', 'none');
+          tooltip_element.css('display', 'none');
       });
       jQuery('svg circle').bind('mousemove', function(e){
-          tooltip.css('left', e.pageX + 10)
+          tooltip_element.css('left', e.pageX + 10)
             .css('top', e.pageY + 10)
             .css('display', 'block')
             .html(jQuery(this).attr('data-tooltip'));
